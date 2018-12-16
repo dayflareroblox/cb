74c0cbecf818acae95ca22b549f23e357289e8cc
discordmania <41619492+discordmania@users.noreply.github.com>
2018-12-16 18:33:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -67,19 +67,11 @@ if(cmd === `${prefix}purge`){
 }   
    
   //---------------------------------------------------------------\\//---------------------------------------------------------------\\
-   if(cmd === `${prefix}whatisblueventus`){
-    let embed = new Discord.RichEmbed()
-    .setDescription("blue ventus is a big gay buthole who fucks trannys and sucks desireds cock because he can and hes a hoe and a hoex3 and then times again by 10. @BlueVentus ")
-    .setColor("#ff001d")
-    
-    message.channel.send(embed)
-   }      
+   
   //---------------------------------------------------------------\\//---------------------------------------------------------------\\  
 if(cmd === `${prefix}a`){
    
-     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Sorry, but you do not have the **Manage Messages** permissions! If you think this is an error, contact an owner.')
-    if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.channel.send('I do not have the **Manage Messages** permission in this server.');   
-    let an = args.join(" ");
+   let an = args.join(" ");
 
    let embed = new Discord.RichEmbed()
    .setTitle("Announcement.")
